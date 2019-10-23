58df7e05d515c999ceacdbffabd70c89cc784691
Kyle Cox <kylecox213@gmail.com>
2019-10-23 00:02:58 -0400

    modifying js across all component pages; styling edits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+import Mixer from "./components/Shaker";
+import monsters from "./monsters.json";
 
-function App() {
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    clickedArr: [],
+    topScore: 0,
+    score: 0,
+    message: " ",
+    wrongGuess: false
+  };
+
+  clickPicture = (id) => {
+    //arrange images in random manner
+    const shuffledArr = this.shuffledArr(friends);
+    this.setState({ friends: shuffledArr });
+    //if clicked image already resets score and emptys array
+    if (this.state.clickedArr.includes(id)) {
+      this.setState({ score: 0, clickedArr: [], message: "You Already Clicked That! Game Over!! Click an Image to Try Again!", wrongGuess: true });
+    } else {
+      this.setState({
+        clickedArr: this.state.clickedArr.concat([id]),
+        score: this.state.score + 1,
+        message: "Good Guess!",
+        wrongGuess: false
+      });
+    }
+    //sets topscore
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+  }
+
+  shuffledArr = (picturesArr) => {
+    for(let i = picturesArr.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random()*(i + 1));
+      [picturesArr[i], picturesArr[j]] = [picturesArr[j], picturesArr[i]];
+    }
+    return picturesArr;
+  }
+  
+
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Memory Game</Title>
+      <Counter>Correct Guesses So Far: <span className="score">{this.state.score} | Top Score: {this.state.topScore}</span>
+        <p className="message">{this.state.message}</p>
+      </Counter>
+      <Shaker
+      shaker = {this.state.wrongGuess}
+      friends =
+        {this.state.friends.map(friend => (
+          <FriendCard
+            clickPicture={this.clickPicture}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+            value={friend.clicky}
+          />
+        ))}
+      />
+    </Wrapper>
   );
 }
+}
+
 
 export default App;
--- my-app/src/Mixer/index.js
+++ my-app/src/Mixer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
 
-const Mixer = props => <div className= {props.mixer===true ? "mixer" : "still"}>{props.monsters}</div>;
+const Mixer = props => <div className= {props.mixer===true ? "mixer" : "still"}>{props.friends}</div>;
 
 export default Mixer;
